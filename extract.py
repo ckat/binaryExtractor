c771fb967da345c3a12c00ed1fc81e6a13ca8e66
mgr <236_@udm.ru>
2015-07-06 22:50:44 +0400

    Initial commit

---
+++ extract.py
@@ -0,0 +1,25 @@
+import lxml.etree as etree;
+import base64
+import os
+
+
+def extractPdf (type, dataBlocks, startIndex, id):
+	i = startIndex;
+	for dataBlock in dataBlocks:
+		i = i+1;
+		fileName = "result_" + type + "_" + str(id) + "_" + str(i) +".pdf";
+		print("Decoding to: " + fileName);
+		f = open(fileName, 'w');
+		decoded = base64.b64decode(dataBlock)
+		f.write(decoded)
+	return i - startIndex;
+
+root = 	etree.parse("sample2.xml");
+pid = os.getpid() # just a unique enough id 
+NSMAP = {'ent': 'http://omsoe.crm.tmobile.net/datatypes/entity'}
+orderImages = root.xpath("//ent:OrderImage/ent:data/text()", namespaces = NSMAP)
+receipts = root.xpath("//ent:SalesPartnerOrderReceipt/ent:data/text()", namespaces = NSMAP)
+count = 0;
+count += extractPdf("OrderImage", orderImages, 0, pid);
+count += extractPdf("SalesPartnerOrderReceipt",receipts, count, pid);
+print("Extracted " + str(count) + " data block(s)")
---
+++ readme.md
@@ -0,0 +1,2 @@
+##Purpose##
+Extract base64 encoded data from SOAP logs using XPATH
