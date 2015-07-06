e9821a94d6c995cbe4412838eb6575a73ce72d7f
mgr <236_@udm.ru>
2015-07-06 23:02:55 +0400

    cleanup

--- extract.py
+++ extract.py
@@ -3,11 +3,11 @@ import base64
 import os
 
 
-def extractPdf (type, dataBlocks, startIndex, id):
+def extractPdf (type, dataBlocks, startIndex, runId):
 	i = startIndex;
 	for dataBlock in dataBlocks:
 		i = i+1;
-		fileName = "result_" + type + "_" + str(id) + "_" + str(i) +".pdf";
+		fileName = "result_" + type + "_" + str(runId) + "_" + str(i) +".pdf";
 		print("Decoding to: " + fileName);
 		f = open(fileName, 'w');
 		decoded = base64.b64decode(dataBlock)
@@ -20,6 +20,6 @@ NSMAP = {'ent': 'http://omsoe.crm.tmobile.net/datatypes/entity'}
 orderImages = root.xpath("//ent:OrderImage/ent:data/text()", namespaces = NSMAP)
 receipts = root.xpath("//ent:SalesPartnerOrderReceipt/ent:data/text()", namespaces = NSMAP)
 count = 0;
-count += extractPdf("OrderImage", orderImages, 0, pid);
+count += extractPdf("OrderImage", orderImages, count, pid);
 count += extractPdf("SalesPartnerOrderReceipt",receipts, count, pid);
 print("Extracted " + str(count) + " data block(s)")
